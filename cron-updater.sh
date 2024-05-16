69272f0e107e522b1116688eb139316b3475f5ba
Piotr Raszkowski <piotr@raszkowski.pl>
2024-05-16 13:25:05 +0200

    fix

--- cron-updater.sh
+++ cron-updater.sh
@@ -48,17 +48,17 @@ if [ -f "$miner/config.json" ]; then
     echo "Price threshold is set to $priceThresholdMin"
 
     apiLoginResponse="$(/usr/bin/curl -s -d '{"userName":"guest@qubic.li", "password":"guest13@Qubic.li"}' -H "Content-Type: application/json" -X POST 'https://api.qubic.li/Auth/Login')"
-    apiToken=$(echo "$apiLoginResponse" | jq -r .token)
-    tickOverviewResponse=$(/usr/bin/curl -s -H "Authorization: Bearer $apiToken" -H "Content-Type: application/json" 'https://api.qubic.li/Network/TickOverview?epoch=&offset=0')
-    tickOverviewPrice=$(echo "$tickOverviewResponse" | jq -r .price)
+    #apiToken=$(echo "$apiLoginResponse" | jq -r .token)
+    #tickOverviewResponse=$(/usr/bin/curl -s -H "Authorization: Bearer $apiToken" -H "Content-Type: application/json" 'https://api.qubic.li/Network/TickOverview?epoch=&offset=0')
+    #tickOverviewPrice=$(echo "$tickOverviewResponse" | jq -r .price)
 
-    echo "Current price is: $tickOverviewPrice"
+    #echo "Current price is: $tickOverviewPrice"
 
-    if (( $(echo "$tickOverviewPrice >= $priceThresholdMin" | bc -l) )); then
-      enabled="true"
-    else
-      enabled="false"
-    fi
+    #if (( $(echo "$tickOverviewPrice >= $priceThresholdMin" | bc -l) )); then
+    #  enabled="true"
+    #else
+    #  enabled="false"
+    #fi
   fi
 else
   enabled="true"
