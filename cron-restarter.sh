7159cfc198d4c55a987e874b764e3586f7024d09
Piotr Raszkowski <piotr@raszkowski.pl>
2024-05-16 11:43:54 +0200

    fix

--- cron-restarter.sh
+++ cron-restarter.sh
@@ -25,8 +25,7 @@ echo "Current directory: $(pwd)"
 echo "Updating $miner in $(pwd)..."
 
 echo "Pulling latest changes..."
-HOME=/root
-echo "Home directory: $(HOME)"
+eval `keychain --eval --agents ssh id_ed25519`
 /usr/bin/git pull
 
 if [ ! -d "$miner" ]; then
