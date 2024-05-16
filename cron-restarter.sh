bfab128b71ea1c08b7415bd99268161a41311454
Piotr Raszkowski <piotr@raszkowski.pl>
2024-05-16 14:32:20 +0200

    fix

--- cron-restarter.sh
+++ cron-restarter.sh
@@ -37,9 +37,35 @@ if [ ! -d "$miner" ]; then
 fi
 
 if [ -f "$miner/config.json" ]; then
-    enabled="$(jq .enabled "$miner/config.json")"
+  cat "$miner/config.json"
+
+  enabled="$(jq .enabled "$miner/config.json")"
+
+  priceThresholdEnabled="$(jq .priceThresholdEnabled "$miner/config.json")"
+  if [ "$priceThresholdEnabled" == "true" ]; then
+    priceThresholdMin="$(jq -r .priceThresholdMin "$miner/config.json")"
+
+    echo "Price threshold is set to $priceThresholdMin"
+
+    apiLoginResponse="$(/usr/bin/curl -s -d '{"userName":"guest@qubic.li", "password":"guest13@Qubic.li"}' -H "Content-Type: application/json" -X POST https://api.qubic.li/Auth/Login)"
+    apiToken=$(echo "$apiLoginResponse" | jq -r .token)
+    tickOverviewResponse=$(/usr/bin/curl -s -H "Authorization: Bearer $apiToken" -H "Content-Type: application/json" 'https://api.qubic.li/Network/TickOverview?epoch=&offset=0')
+    tickOverviewPrice=$(echo "$tickOverviewResponse" | jq -r .price)
+
+    echo "Current price is: $tickOverviewPrice"
+
+    if awk "BEGIN {exit !($tickOverviewPrice >= $priceThresholdMin)}"; then
+      echo "$tickOverviewPrice is ge $priceThresholdMin, miner will be enabled"
+      enabled="true"
+    else
+      echo "$tickOverviewPrice is lt $priceThresholdMin, miner will be disabled"
+      enabled="false"
+    fi
+  else
+    echo "Price threshold validation is not enabled, skipping."
+  fi
 else
-    enabled="true"
+  enabled="true"
 fi
 
 echo "Enabled = $enabled"
